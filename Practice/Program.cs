26c10aada0568f81867dbecc963d8f697e297835
ArturIshmaev <arturishmaev@yandex.ru>
2022-11-09 15:20:24 +0500

    Добавили сортировку пузырьком

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Сортировка выбором.
-System.Console.WriteLine("Введите количестов элементов массива:");
+/*System.Console.WriteLine("Введите количестов элементов массива:");
 int n = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[n];
 for (int i = 0; i < n; i++)
@@ -24,4 +24,30 @@ for (int i = 0; i < n - 1; i++)
      array[minIndex] = array[i];
      array[i] = temp;
 }
-System.Console.WriteLine("Отсортированый массив: [" + string.Join(", ", array)+ "]");
+System.Console.WriteLine("Отсортированый массив: [" + string.Join(", ", array)+ "]");*/
+
+// Сортировка пузырьком
+
+System.Console.WriteLine("Введите количестов элементов массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    System.Console.Write("Введите значение элемента массива :");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+System.Console.WriteLine();
+System.Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n - 1; j++)
+    {
+        if (array[j] > array[j + 1])
+        {
+            int temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+        }
+    }
+}
+ System.Console.WriteLine("Отсортированый массив: [" + string.Join(", ", array)+ "]");
