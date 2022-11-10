ecf163a620c622a6df70d6d5beb656ec4583c798
ArturIshmaev <arturishmaev@yandex.ru>
2022-11-10 21:48:20 +0500

    Добавили быструю сортировку

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Сортировка выбором.
-/*System.Console.WriteLine("Введите количестов элементов массива:");
+
+System.Console.WriteLine("Введите количестов элементов массива:");
 int n = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[n];
 for (int i = 0; i < n; i++)
@@ -24,9 +25,9 @@ for (int i = 0; i < n - 1; i++)
      array[minIndex] = array[i];
      array[i] = temp;
 }
-System.Console.WriteLine("Отсортированый массив: [" + string.Join(", ", array)+ "]");*/
+System.Console.WriteLine("Отсортированый массив: [" + string.Join(", ", array)+ "]");
 
-// Сортировка пузырьком
+// Сортировка пузырьком.
 
 System.Console.WriteLine("Введите количестов элементов массива:");
 int n = Convert.ToInt32(Console.ReadLine());
@@ -51,3 +52,55 @@ for (int i = 0; i < n; i++)
     }
 }
  System.Console.WriteLine("Отсортированый массив: [" + string.Join(", ", array)+ "]");
+
+// Быстрая сортировка.
+
+System.Console.WriteLine("Введите количестов элементов массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    System.Console.Write("Введите число :");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+System.Console.WriteLine();
+System.Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+
+void QuickSort(int[] array, int minIndex, int maxIndex)
+{
+    if (minIndex >= maxIndex)
+    {
+        return;
+    }
+    int pivot = GetPivotIndex(array, minIndex, maxIndex);
+    QuickSort(array, minIndex, pivot - 1);
+    QuickSort(array, pivot + 1, maxIndex);
+    return;
+}
+
+int GetPivotIndex(int[] array, int minIndex, int maxIndex)
+{
+    int pivotIndex = minIndex - 1;
+    for (int i = minIndex; i < maxIndex; i++)
+    {
+        if (array[i] < array[maxIndex])
+        {
+            pivotIndex++;
+            Swap(array, i, pivotIndex);
+        }
+    }
+    pivotIndex++;
+    Swap(array, pivotIndex, maxIndex);
+    return pivotIndex;
+}
+
+void Swap(int[] array, int leftValue, int rightValue)
+{
+    int temp = array[leftValue];
+    array[leftValue] = array[rightValue];
+    array[rightValue] = temp;
+
+}
+QuickSort(array, 0, array.Length - 1);
+System.Console.WriteLine("Отсортированый массив: [" + string.Join(", ", array) + "]");
